5ecddcd3a70be45530c8e61e67775822500ab406
Brandon Holz <brandon@fyve.co.za>
2021-12-04 13:51:01 +0200

    Create useSupabase hook

---
+++ utils/useSupabase.js
@@ -0,0 +1,16 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_API_KEY
+);
+
+const useSupabase = () => {
+  const [session, setSession] = React.useState(supabase.auth.session());
+
+  supabase.auth.onAuthStateChange(async (_event, session) => {
+    setSession(session);
+  });
+
+  return { session, supabase };
+};
