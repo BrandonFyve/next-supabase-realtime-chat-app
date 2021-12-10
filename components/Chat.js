e9b5c20455cb7c78f2653096b44a88d567d52bb8
Brandon Holz <brandon@fyve.co.za>
2021-12-11 01:02:51 +0200

    New message form

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-const Chat = ({ supabase }) => {
+const Chat = ({ session, supabase }) => {
   const [messages, setMessages] = React.useState([]);
+
+  const message = React.useRef();
 
   React.useEffect(async () => {
     const getMessages = async () => {
@@ -25,11 +27,36 @@ const Chat = ({ supabase }) => {
     await setupMessageSubscription();
   }, []);
 
+  const sendMessage = async (evt) => {
+    evt.preventDefault();
+
+    try {
+      const content = message.current.value;
+
+      await supabase
+        .from("message")
+        .insert([{ content, user_id: session.user.id }]);
+
+      message.current.value = "";
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
       {messages.map((message) => (
         <div key={message.id}>{message.content}</div>
       ))}
+      <form onSubmit={sendMessage}>
+        <input
+          required
+          type="text"
+          placeholder="Write your message"
+          ref={message}
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 };
